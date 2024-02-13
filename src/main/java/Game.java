3d3d9668382de2f673164f0f9ec3a4154e21c49c
Jack-Thompson07 <159587261+Jack-Thompson07@users.noreply.github.com>
2024-02-13 22:11:16 +0000

    Added Game class

---
+++ src/main/java/Game.java
@@ -0,0 +1,14 @@
+public class Game {
+
+  private Player p1;
+  private Player p2;
+
+  public Game(Player p1, Player p2) {
+    this.p1 = p1;
+    this.p2 = p2;
+  }
+
+  public void playGame() {
+
+  }
+}
